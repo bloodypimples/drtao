48ff3d5e6695820143da7061df492dc4c3200f15
Hieu <hieudoan2609@gmail.com>
2018-01-25 23:12:14 +0700

    disable validation for image uploads

--- app/models/part.rb
+++ app/models/part.rb
@@ -3,4 +3,5 @@ class Part < Searchable
   has_many :comments, dependent: :destroy
   has_attached_file :image, styles: { large: "1300x1300>", medium: "600x600>", thumb: "100x100" }, default_url: "/assets/default.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  do_not_validate_attachment_file_type :image
 end
