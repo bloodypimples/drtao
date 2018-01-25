002aa4e7cbef7cdde0c2228dc9e377f8ddfb5bce
Hieu <hieudoan2609@gmail.com>
2018-01-26 00:14:09 +0700

    change home page category

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @ep_kinh_iphone_services = get_from_category("Ép Kính iPhone", 4, "services")
+    @ep_kinh_iphone_services = get_from_category("Ép kính iPhone", 4, "services")
     @sua_iphone_services = get_from_category("Sửa iPhone", 4, "services")
     @iphone_moi_products = get_from_category("iPhone mới", 4, "products")
     @iphone_cu_products = get_from_category("iPhone cũ", 4, "products")
