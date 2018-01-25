25c466cf13a054bf7ce9dc6c5fd2fbc697f208f4
Hieu <hieudoan2609@gmail.com>
2018-01-25 22:56:14 +0700

    re-enabling content type validation

--- app/models/part.rb
+++ app/models/part.rb
@@ -2,5 +2,5 @@ class Part < Searchable
   belongs_to :category
   has_many :comments, dependent: :destroy
   has_attached_file :image, styles: { large: "1300x1300>", medium: "600x600>", thumb: "100x100" }, default_url: "/assets/default.png"
-  # validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
