b401417a44f00645d5e1b8694ff37195481d8aca
Hieu <hieudoan2609@gmail.com>
2018-01-29 04:41:48 +0700

    add compression for general images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,19 @@
 class Image < ApplicationRecord
-  has_attached_file :image, styles: { large: "1300x1300>", medium: "600x600>", thumb: "100x100" }, default_url: "/assets/default.png"
+  after_commit :compress_image, on: [:create, :update]
+
+  has_attached_file :image, styles: { large: ["500x500>", :jpg], medium: ["250x250>", :jpg], thumb: ["100x100", :jpg] }, default_url: "/assets/default.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  def compress(image_path)
+    system "convert #{image_path} -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace sRGB #{image_path}"
+    # image = MiniMagick::Image.open(image_path)
+    # image.strip()
+    # image.write image_path
+  end
+
+  def compress_image
+    compress(self.image.path(:large))
+    compress(self.image.path(:medium))
+    compress(self.image.path(:thumb))
+  end
 end
