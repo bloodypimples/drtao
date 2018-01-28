b55fd2ce9e17495f92555c7b8524187fc940aeb5
Hieu <hieudoan2609@gmail.com>
2018-01-28 15:53:23 +0700

    add png compression

--- app/models/searchable.rb
+++ app/models/searchable.rb
@@ -1,7 +1,19 @@
 class Searchable < ApplicationRecord
+  after_save :compress
+
+  has_attached_file :image, styles: { large: ["1300x1300>", :png], medium: ["600x600>", :png], thumb: ["100x100", :png] }, default_url: "/assets/default.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
   self.abstract_class = true
 
   def self.search(name)
     where("name ILIKE ?", "%#{name}%")
+  end
+
+  def compress
+    system "convert #{self.image.path(:original)} -strip #{self.image.path(:original)}"
+    system "convert #{self.image.path(:large)} -strip #{self.image.path(:large)}"
+    system "convert #{self.image.path(:medium)} -strip #{self.image.path(:medium)}"
+    system "convert #{self.image.path(:thumb)} -strip #{self.image.path(:thumb)}"
   end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,4 @@
 class Service < Searchable
   belongs_to :category
   has_many :comments, dependent: :destroy
-  has_attached_file :image, styles: { large: "1300x1300>", medium: "600x600>", thumb: "100x100" }, default_url: "/assets/default.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
