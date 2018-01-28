cee340518adb25c4bc3f665533de128597305c6d
Hieu <hieudoan2609@gmail.com>
2018-01-28 17:51:12 +0700

    fix jpg compression hook method

--- app/models/searchable.rb
+++ app/models/searchable.rb
@@ -1,5 +1,5 @@
 class Searchable < ApplicationRecord
-  after_save :compress
+  after_commit :compress
 
   has_attached_file :image, styles: { large: ["1300x1300>", :jpg], medium: ["600x600>", :jpg], thumb: ["100x100", :jpg] }, default_url: "/assets/default.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
@@ -13,6 +13,7 @@ class Searchable < ApplicationRecord
   def compress
     system "convert #{self.image.path(:large)} -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace sRGB #{self.image.path(:large)}"
     system "convert #{self.image.path(:medium)} -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace sRGB #{self.image.path(:medium)}"
-    system "convert #{self.image.path(:medium)} -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace sRGB #{self.image.path(:medium)}"
+    system "convert #{self.image.path(:thumb)} -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace sRGB #{self.image.path(:thumb)}"
+    system "echo 'executed'"
   end
 end
